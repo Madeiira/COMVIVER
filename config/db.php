559f4a22974df15f8427644fa2702297b350fcf8
ganjos <gmmanjos@gmail.com>
2025-08-19 16:53:31 -0300

    Main - Configurando porta correta MYSQL

--- config/db.php
+++ config/db.php
@@ -1,22 +1,7 @@
-/*
-Estrutura do Banco de dados
-    CREATE DATABASE crud_contatos;
-    USE crud_contatos;
 
-    CREATE TABLE contatos (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        nome VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL,
-        telefone VARCHAR(20) NOT NULL,
-        cidade VARCHAR(50) NOT NULL,
-        data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
-*/
-
-// Conexão padrão BD
 
 <?php
-$host = "localhost";
+$host = "localhost:3320";
 $user = "root";
 $pass = "";
 $db = "crud_contatos";
